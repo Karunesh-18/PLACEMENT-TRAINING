54014a7b5c4418cc6b603c9876a42be1aa8b53d3
Karuensh <karuneshramesh182@gmail.com>
2026-02-23 10:36:30 +0530

    Refactor left-angled pattern printing logic and add inverted mirrored pattern program

--- JAVA/left_angled.java
+++ JAVA/left_angled.java
@@ -7,8 +7,8 @@ public class left_angled {
             System.out.print("Enter the number of rows: ");
             int n = sc.nextInt();
 
-            for (int i = n; i >= 1; i--) {
-                for (int j = 1; j <= n - i; j++) {
+            for (int i = 1; i <= n; i++) {
+                for(int j = 1; j<= n - i; j++) {
                     System.out.print("  ");
                 }
                 for (int k = 1; k <= i; k++) {
