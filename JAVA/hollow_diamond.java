c8fc82462845de5e3255eefb992771ca864a7cea
Karuensh <karuneshramesh182@gmail.com>
2026-02-23 12:41:27 +0530

    Add pattern printing programs for diamond, hollow diamond, hollow square, pyramid, and triangle

---
+++ JAVA/hollow_diamond.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class hollow_diamond {
+    @SuppressWarnings("resource")
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }   
+            for(int k = 1; k <= (2 * i - 1); k++){
+                if(k == 1 || k == (2 * i - 1)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i = n-1; i >= 1; i--){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }   
+            for(int k = 1; k <= (2 * i - 1); k++){
+                if(k == 1 || k == (2 * i - 1)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
